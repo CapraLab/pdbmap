fcbc32522c9aa853659af92374079edf1d84e6fc
Mike Sivley <mike.sivley@vanderbilt.edu>
2013-07-09 12:17:37 -0500

    Further update to analyze_conflicts.py

---
+++ analyze_conflicts.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+#PBS -M mike.sivley@vanderbilt.edu
+#PBS -m bae
+#PBS -l mem=5000mb
+#PBS -l walltime=5:00:00:00
+
+cd /labs/twells/sivleyrm/pdbmap
+./analyze_conflicts.py -c analyze_all_pdb.config > pdbmap_conflict_graphic.txt 2>pdbmap_conflict_positions.txt
