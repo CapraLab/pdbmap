f4fd3283a005c8b2c0ae92c3bb566791c574e114
Mike Sivley <mike.sivley@vanderbilt.edu>
2014-01-14 17:00:30 -0600

    Updated gitignore

--- qsub/load_bed.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#PBS -M mike.sivley@vanderbilt.edu
-#PBS -m bae
-#PBS -l nodes=vision.mc.vanderbilt.edu
-#PBS -l mem=10000mb
-#PBS -l walltime=5:00:00:00
-
-cd /labs/twells/sivleyrm/pdbmap
-./load_bed.py /scratch/sivleyrm/pdbmap/variants/amd_exomechip.bed /scratch/sivleyrm/pdbmap/maps/pdbmap_v7.bed /scratch/sivleyrm/pdbmap/intersections/ ExomechipAMD
--- qsub/load_bed_1kgenomes.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#PBS -M mike.sivley@vanderbilt.edu
-#PBS -m bae
-#PBS -l nodes=vision.mc.vanderbilt.edu
-#PBS -l mem=15000mb
-#PBS -l walltime=5:00:00:00
-
-cd /projects/Bush_eQTL/sivleyrm/projects/pdbmap
-lib/load_bed.py /scratch/sivleyrm/pdbmap/variants/1kgenomes_0-indexed.bed /scratch/sivleyrm/pdbmap/maps/pdbmap_v7_3.bed /scratch/sivleyrm/pdbmap/intersections 1kGenomesReMerge
