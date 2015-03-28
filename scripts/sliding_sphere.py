bccb0b4f823f86b46392a32454f04ebebe13d9f1
Mike Sivley <mike.sivley@vanderbilt.edu>
2015-03-28 10:51:58 -0500

    Debugging Fst addition to sliding sphere. Fixing bug in sliding sphere permutation method

--- scripts/sliding_sphere.py
+++ scripts/sliding_sphere.py
@@ -3,11 +3,12 @@
 # Implementation of the sliding sphere analysis for variant localization 
 # and population differentiation.
 
-PERMUTATIONS = 999
+PERMUTATIONS = 99
 
 # All processes use the same seed
 # Change the seed if the process load is imbalanced
 import numpy as np
+import random
 np.random.seed(5)
 random.seed(5)
 
@@ -15,7 +16,7 @@ from profilehooks import profile
 np.set_printoptions(threshold='nan')
 from scipy.spatial import KDTree
 from scipy.stats import chisquare,fisher_exact,describe
-import sys,os,csv,time,math,random,copy,gzip
+import sys,os,csv,time,math,copy,gzip
 np.seterr(all='ignore')
 import MySQLdb, MySQLdb.cursors
 from warnings import filterwarnings,resetwarnings
@@ -91,8 +92,8 @@ def main(ppart=0,ppidx=0,structid=None,radius=15):
       if verbose:
         print "%s.%s"%(structid,biounit)
 
-      if os.path.exists(obs_file) and os.path.exists(perm_file):
-        continue # skip if this biological asssembly has been processed
+      # if os.path.exists(obs_file) and os.path.exists(perm_file):
+      #   continue # skip if this biological asssembly has been processed
 
       # Load the structure
       residues,nbrs3D,nbrs1D = load_structure(structid,biounit,radius,verbose)
@@ -107,7 +108,7 @@ def main(ppart=0,ppidx=0,structid=None,radius=15):
           sys.stdout.flush()
         # Calculate sliding sphere over permuted SNP assignments
         stats = sliding_sphere(perm_residues,(nbrs3D,nbrs1D),radius,verbose)
-        pspheres = [perm_residues[j] + stat for j,stat in enumerate(stats)]
+        pspheres = [perm_residues[j][:16] + perm_residues[j][-3:] + stat for j,stat in enumerate(stats)]
         perm_spheres.append(pspheres)
       perm_spheres = np.array(perm_spheres)
       if verbose:
@@ -120,7 +121,7 @@ def main(ppart=0,ppidx=0,structid=None,radius=15):
 
       # Run the sliding sphere analysis on the observed values
       stats = sliding_sphere(residues,(nbrs3D,nbrs1D),radius,verbose)
-      spheres = np.array([residues[i] + stat for i,stat in enumerate(stats)])
+      spheres = np.array([residues[i][:16] + residues[i][-3:] + stat for i,stat in enumerate(stats)])
 
       # Total descriptors:   19
       # Total measurements: 122
@@ -179,8 +180,6 @@ def sliding_sphere(residues,nbrs,radius,verbose=False):
 def sphere(r,residues,nbrs,radius):
   nbrs3D,nbrs1D = nbrs
   sphere   = isolate_sphere(r,residues,radius,nbrs3D)
-  print "\nSphere:"
-  print sphere
   scounts  = sphere_count(sphere) # Residue and variant counts (by pop)
   sdaf     = daf(sphere)     # Calculate DAF
   scdaf    = cumdaf(sdaf)    # Cumulative frequency (by pop)
@@ -235,16 +234,14 @@ def sphere_count(residues):
   return [numres,allsnp]+numsnp+shrsnp
 
 def fst_roi(residues):
-  if not residues: return np.repeat([None],10)
-  print "\n+~+~+~+~+~+~+~+~+~+~+~+~+~+~+~+~+"
-  print residues.shape
-  nhat_agg = np.sum([[r[i] for i in range(16,27)] for r in residues if r[1]],axis=0)
-  dhat_agg = np.sum([[r[i] for r in range(27,37)] for r in residues if r[1]],axis=0)
-  print nhat_agg.shape
-  print dhat_agg.shape
-  print (nhat_agg/dhat_agg).shape
-  print nhat_agg/dhat_agg
-  return (nhat_agg / dhat_agg).to_list()
+  # Replace None values with np.nan
+  fstvals = np.array([[r[i] if r[i] else np.nan for i in range(16,38)] for r in residues if r[1]])
+  # Sphere isn't empty and isn't full of NaN and there are >1 SNPs in the sphere
+  if not np.any(fstvals) or np.all(np.isnan(fstvals)) or fstvals.shape[0]<2:
+    return [np.nan for i in range(11)]
+  nhat_agg = np.sum([[r[i] for i in range(0 ,11)] for r in fstvals],axis=0).flatten()
+  dhat_agg = np.sum([[r[i] for i in range(11,22)] for r in fstvals],axis=0).flatten()
+  return (nhat_agg / dhat_agg).tolist()
 
 def cumdaf(dafs):
   if not dafs: return [0,0,0,0,0]
@@ -266,7 +263,7 @@ def ddaf_roi(ddafs):
   # Sphere isn't empty and isn't full of NaN and there are >1 SNPs in the sphere
   if not np.any(ddafs) or np.all(np.isnan(ddafs)) or ddafs.shape[0]<2:
     # Null values for 10 pop combos (i) and 3 metrics (j)
-    return [0 for i in range(10) for j in range(3)] # 0 for each population pair
+    return [np.nan for i in range(10) for j in range(3)] # 0 for each population pair
   # Calculate pop1 mean deltaDAF
   y = np.copy(ddafs)
   # If a column has <2 SNPs, don't calculate the aggregate dDAF for that column
@@ -308,7 +305,8 @@ def permute_snps(residues,permutations=1000):
   locseen_add,snpseen_add = locseen.add,snpseen.add
   loc_mat = []    # Matrix of variant coordinates (3D)
   snps    = []    # List of missense SNPs
-  mafs    = []    # List of minor allele frequencies. Order matches snps
+  mafs    = []    # List of SNP allele frequencies. Order matches snps
+  fsts    = []    # List of SNP Fst values (nhat,dhat). Order matches snps
   snpidx  = []    # (unp,seqid) for each variant in this structure
   residx  = {}    # (unp,seqid)->[residue-index] for each residue in loc_mat
   for r in residues:
@@ -316,17 +314,18 @@ def permute_snps(residues,permutations=1000):
     if not (tuple(r[-3:]) in locseen or locseen_add(tuple(r[-3:]))):
       loc_mat.append(r[-3:])
       # Assign this structural location index to a unique sequence residue
-      if r[-5:-3] not in residx:
-        residx[r[-5:-3]] = []
-      residx[r[-5:-3]].append(len(loc_mat)-1)
+      if r[13:15] not in residx:
+        residx[r[13:15]] = []
+      residx[r[13:15]].append(len(loc_mat)-1)
       # Check if the location is a variant residue
       if r[1]:
         # Check if a variant has already been mapped to this residue
-        if not (tuple(r[-5:-3]) in snpseen or snpseen_add(tuple(r[-5:-3]))):
+        if not (tuple(r[13:15]) in snpseen or snpseen_add(tuple(r[13:15]))):
           # snps.append(r[2])
-          mafs.append(r[2:10]) # all allele information
+          mafs.append(r[3:8]) # all allele information
+          fsts.append(r[16:-3])
           # Assign this variant to the unique sequence residue
-          snpidx.append(r[-5:-3])
+          snpidx.append(r[13:15])
   # Reduce the location matrix to unique residues (remove multi-variant mappings)
   loc_mat  = [list(x) for x in loc_mat]
   rescount = len(loc_mat) # Number of residues
@@ -336,22 +335,31 @@ def permute_snps(residues,permutations=1000):
   for i in range(permutations):
     # Reset SNP assignments
     for r in perm_residues:
-      r[1] = 0
-      r[2:10] = [None,0,0,0,0,None,None,None]
+      r[1]     = 0 # reset SNP flag
+      r[3:8]   = [None for i in range(5)]  # reset MAFs
+      r[16:-3] = [None for i in range(22)] # reset Fsts
     # Permute the snpidx assignments, propagate through residx to loc_mat
     # More accurately models plausible variant distribution
     perm_snpidx   = random.sample(residx.keys(),snpcount)
     resseen     = set()
     resseen_add = resseen.add
     for r in perm_residues:
-      if tuple(r[-5:-3]) in perm_snpidx:
-        r[1] = 1
-        resseen_add(tuple(r[-5:-3]))
-        r[2:10] = list(mafs[list(resseen).index(tuple(r[-5:-3]))])
+      # Assign SNPs to residues
+      if tuple(r[13:15]) in perm_snpidx:
+        resseen_add(tuple(r[13:15]))
+        r[1]     = 1 # set SNP flag
+        r[3:8]   = list(mafs[list(resseen).index(tuple(r[13:15]))]) # assign MAFs
+        r[16:-3] = list(fsts[list(resseen).index(tuple(r[13:15]))]) # assign Fsts
     yield perm_residues
 
 def load_structure(structid,biounit,radius,verbose=False):
   # Load the structure with 1000 Genomes Phase III population allele frequencies
+  # [0:3)   isvar,issnp,name
+  # [3:8)   minor allele frequencies for population
+  # [9:11)  allele information
+  # [11:16) unp/sequence/chain information
+  # [16:-3) Fst nhat/dhat values
+  # [-3:)   Structural coordinates
   q  = "SELECT !ISNULL(c.gc_id) as isvar,c.consequence LIKE '%%missense_variant%%' as issnp,"
   q += "d.name,d.amr_af,d.eas_af,d.sas_af,d.eur_af,d.afr_af,"
   q += "d.da,d.ref_allele,d.alt_allele,e.model,e.chain,e.unp,a.seqid,a.icode,"
