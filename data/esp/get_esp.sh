f7d48571ec4916c267f0dc416fff7ab4150dbfdc
Mike Sivley <mike.sivley@vanderbilt.edu>
2015-04-13 10:25:56 -0500

    Added get script for Exome Sequencing Project data

---
+++ data/esp/get_esp.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# Pulls Exome Sequencing Project, Version 2
+# in VCF format from the Exome Variant Server
+# 6,500 Exomes for Chromosomes 1-22, X, and Y
+# Reference assembly: hg19/GRCh37
+
+# Download the VCF file
+wget -r --no-parent -N --reject -nH -nd --timeout=100000 http://evs.gs.washington.edu/evs_bulk_data/ESP6500SI-V2-SSA137.GRCh38-liftover.snps_indels.vcf.tar.gz
