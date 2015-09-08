3047dbd20a305c89c8c2c02f9727d6ace4791b97
Mike Sivley <mike.sivley@vanderbilt.edu>
2015-09-08 12:45:49 -0500

    Updated Pi analysis to correct for multi-allelic SNPs

--- scripts/pi_analysis.py
+++ scripts/pi_analysis.py
@@ -30,6 +30,7 @@
 #=============================================================================#
 ## Package Dependenecies ##
 import pandas as pd, numpy as np, subprocess as sp
+pd.set_option('display.max_columns', 500)
 import time,os,sys,random,argparse,itertools,csv
 from collections import OrderedDict
 from scipy.spatial import KDTree
@@ -41,6 +42,7 @@ np.nanmean([])
 resetwarnings()
 np.random.seed(10)
 random.seed(10)
+TOL = 0.0000001 # zero tolerance threshold
 #=============================================================================#
 ## Parse Command Line Options ##
 desc   = "Sphere-scan over structure-mapped missense variation, calculating "
@@ -84,19 +86,23 @@ def read_structfile(sfile):
                         ("chain",str),("seqid",int),("icode",str),("x",float),
                         ("y",float),("z",float),("chr",str),("pos",str),
                         ("name",str),("csq",str),("aa",str),("ref",str),
-                        ("maf",float),("geno",str)])
+                        ("pmaf",float),("geno",str)])
   df = pd.read_csv(sfile,sep='\t',skiprows=1,header=None,names=dtypes.keys(),
                     dtype=dtypes,na_values=["NULL"],comment="#")
   df.ix[df["csq"].str.contains("missense_variant",na=False).astype(bool),"csq"] = "m"
   # Convert NaN genotypes to empty strings
   df["geno"] = df["geno"].apply(nan2str)
   df["name"] = df["name"].apply(nan2str)
-  return df.drop_duplicates().reset_index() # catch duplicate residue assignments
+  # Eliminate any SNPs mapped through multiple transcripts to the same residue
+  df = df.drop_duplicates(["structid","biounit","model","chain","seqid","icode","name"])
+  return df.drop_duplicates().reset_index() # catch remaining duplicate residue assignments
 
 def altcheck(genstr):
-  """ Flips ref/alt for any SNPs at frequency >50% """
+  """ Corrects multi-allele SNPs and flips ref/alt for any SNPs at frequency >50% """
+  genstr = ''.join([x if int(x)<=1 else "1" for x in genstr])
   if genstr.count("1") > genstr.count("0"):
     return ''.join("1" if gen=="0" else "0" for gen in genstr)
+  else: return genstr
 
 def maf(genstr):
   """ Calculates allele frequency over observed genotypes """
@@ -113,18 +119,17 @@ def maf_filter(pop1,pop2,maf=0.05):
 
 def prune_mono(pop1,pop2):
   """ Reclassify sites that are monomorphic and equal in both populations """
-  tol = 0.000000001 # zero tolerance threshold
-  for snp in pop1.ix[(pop1["maf"]<tol) & (pop2["maf"]<tol),"name"]:
+  for snp in pop1.ix[(pop1["maf"]<TOL) & (pop2["maf"]<TOL),"name"]:
     if snp:
       print "Filtered (Monomorphic):\t%s"%snp
-  pop1.ix[(pop1["maf"]<tol) & (pop2["maf"]<tol),"csq"] = "monomorphic"
-  pop2.ix[(pop1["maf"]<tol) & (pop2["maf"]<tol),"csq"] = "monomorphic"
+  pop1.ix[(pop1["maf"]<TOL) & (pop2["maf"]<TOL),"csq"] = "monomorphic"
+  pop2.ix[(pop1["maf"]<TOL) & (pop2["maf"]<TOL),"csq"] = "monomorphic"
   return pop1,pop2
 
 def gen2mat(genos):
+  """ Converts a series of genotype strings to a sample x genotype matrix """
   if genos.empty:
     return np.array([])
-  """ Converts a series of genotype strings to a sample x genotype matrix """
   def str2lst(s):
     return list(np.uint8(x) for x in s)
   return np.array(list(genos.apply(str2lst).values),dtype=np.uint8)
@@ -141,39 +146,10 @@ def pi(genos):
   f = c.astype(np.float64) / s
   return np.mean([f[i]*f[j]*(g[i]!=g[j]).sum() for i,j in itertools.combinations(xrange(g.shape[0]),2)])
 
-# def safe_chisquare(obs,exp):
-#   """ Only calculates Chi Square for valid contingency tables """
-#   from scipy.stats import chisquare
-#   return chisquare(obs,exp)
-#   # if obs[0]<5 or obs[1]<5 or exp[0]<5 or exp[0]<5:
-#   #   return np.nan,np.nan
-#   # else:
-#   #   return chisquare(obs,exp)
-
 def multidigit_rand(digits):
   randlist = [random.randint(1,10) for i in xrange(digits)]
   multidigit_rand = int(''.join([str(x) for x in randlist]))
   return multidigit_rand
-
-# def cfet(tables):
-#   """ External call to continuous Fisher's Exact Test (cfet) """
-#   ids = np.zeros((tables.shape[0],1))
-#   tables = np.c_[ids,tables]
-#   tname = "%s.cfet"%multidigit_rand(10)
-#   with open(tname,'wb') as fout:
-#     writer = csv.writer(fout,delimiter='\t')
-#     writer.writerows(tables)
-#   p = sp.Popen(["cfet","-s","-i",tname],stdout=sp.PIPE,stderr=sp.PIPE)
-#   out,err = p.communicate()
-#   try:
-#     os.remove(tname)
-#   except:
-#     pass
-#   if "error" in out:
-#     sys.stderr.write("Error in CFET:\n")
-#     sys.stderr.write(out)
-#     raise Exception("Error in CFET")
-#   return np.array([[float(col) for col in row.split()[1:]] for row in out.strip().split('\n')],dtype=np.float64)
 
 def def_spheres(df,csq=('m','s'),r=10.):
   """ Identifies all SNPs within a radius around each residue """
@@ -192,8 +168,12 @@ def def_spheres(df,csq=('m','s'),r=10.):
   dft["nbridx"]  = [[i for i in kdt.query_ball_point(coord,r) if i in idx] for \
                       _,coord in dft[["x","y","z"]].iterrows()]
   # Gather the names of each variant within the sphere
+  print "\nGathering the neighbor SNPs for 47.A"
+  print [dft.loc[x["nbridx"],:] for _,x in dft.iterrows() if x["seqid"]==47 and x["chain"]=="A"]
   dft["nbrsnps"] = [','.join([str(n) for n in dft.loc[x["nbridx"],"name"]]) for \
                       _,x in dft.iterrows()]
+  # Count the number of SNPs in each sphere
+  dft["snpcnt"]  = dft["nbridx"].apply(lambda x: 0 if not x else len(x))
   # Identify all SNPs residing outside the sphere
   dft["outidx"] = [[i for i in idx if i not in x["nbridx"]] for _,x in dft.iterrows()]
   return dft
@@ -225,7 +205,9 @@ for s1,s2 in structs:
     tres  = pop1.shape[0]
     # Recalculate minor allele frequencies over observed genotypes
     pop1["maf"] = pop1["geno"].apply(maf)
+    print "\n1: Reported and observed allele frequency differs by >5%% at %d sites."%(abs(pop1["pmaf"]-pop1["maf"])>0.05).sum()
     pop2["maf"] = pop2["geno"].apply(maf)
+    print "\n2: Reported and observed allele frequency differs by >5%% at %d sites."%(abs(pop2["pmaf"]-pop2["maf"])>0.05).sum()
     # Identifying population-monorphic sites
     pop1,pop2 = prune_mono(pop1,pop2)
     # Identifying shared common sites (default 1.0 - no filter)
@@ -233,27 +215,38 @@ for s1,s2 in structs:
     # Verify that the structure contains polymorphic residues
     if pop1[pop1["csq"]=='m'].empty:
       if pop1[pop1["csq"]=='common'].empty:
-        print "Structure contains no polymorphic residues"
+        print "Skipped %s,%s: Structure contains no polymorphic residues"%(s1,s2)
       else:
-        print "All polymorphic sites exceed MAF threshold"
+        print "Skipped %s,%s: All polymorphic sites exceed MAF threshold"%(s1,s2)
       continue
     # Define the spheres for missense variants (nsSNPs)
     print "Defining spheres..."
     sph1  = def_spheres(pop1,'m',args.radius)
+    print "\nSphere 47 AFR:"
+    print sph1[(sph1["seqid"]==47) & (sph1["chain"]=="A")]
+    idx47 = np.argmax((sph1["seqid"]==47) & (sph1["chain"]=="A")) # what position is 47 in?
     sph2  = def_spheres(pop2,'m',args.radius)
     # Record the number of residues within each sphere (equal across populations)
     nres  = sph1["nres"].astype(np.float64).values
+    print "\nResidues in sphere 47: %d"%nres[idx47]
     # Initialize the genotype matrix for all spheres
     print "Defining genotype matrices for each sphere..."
     gen1  = [gen2mat(sph1.loc[sph["nbridx"]]["geno"]) for _,sph in sph1.iterrows()]
+    print "\nGenotype matrix of sphere 47:"
+    print gen1[idx47]
     gen2  = [gen2mat(sph2.loc[sph["nbridx"]]["geno"]) for _,sph in sph2.iterrows()]
     # Calculate overall alternate allele counts for each population
     print "Geno1 shape: %s"%len(gen1)
     cnt1 = np.array([gt.sum() for gt in gen1],dtype=np.uint16)
+    print "\nAllele count of sphere 47:"
+    print cnt1[idx47]
     cnt2 = np.array([gt.sum() for gt in gen2],dtype=np.uint16)
     # Calculate overall nucelotide diversity for each population and take the difference
     print "Calculating nucleotide diversity within each sphere..."
     pi1   = np.array([pi(gen) for gen in gen1]) / nres
+    print "\nPi Diversity for sphere 47:"
+    print pi1[idx47]
+    sys.exit()
     pi2   = np.array([pi(gen) for gen in gen2]) / nres
     print "Minimum Pi:",min(pi1.min(),pi2.min())
     dpi   = pi1 - pi2
@@ -286,15 +279,19 @@ for s1,s2 in structs:
     # print "CFETp: %.1e %.1e %.1e %.1e %.1e"%tuple(np.percentile(cfetp,[0,25,50,75,100]))
     print "ChiSq: %.1e %.1e %.1e %.1e %.1e"%tuple(np.percentile(chisq[:,0],[0,25,50,75,100]))
     print "Chip:  %.1e %.1e %.1e %.1e %.1e"%tuple(np.percentile(chisq[:,1],[0,25,50,75,100]))
-    res   = np.vstack((pop1.iloc[:,1:7].values.T,sph1["nbrsnps"].T,sph2["nbrsnps"].T,cnt1,cnt2,pi1,pi2,dpi,Ocnt1,Ocnt2,Opi1,Opi2,fet.T,chisq.T)).T
-    if not np.isnan(res[:,-2].astype(np.float64)).all():
-      print "\nMinimum FET: %s"%res[np.nanargmin(res[:,-2].astype(np.float64)),:]
-      print "Maximum FET: %s\n"%res[np.nanargmax(res[:,-2].astype(np.float64)),:]
-    else:
-      print "\nAll-NaN FET:"
-      np.savetxt(sys.stdout,res[~np.isnan(res[:,-1].astype(np.float64))],fmt="%s",delimiter=' ')
-    names  = ["structid","biounit","model","chain","seqid","icode","nbrsnps1","nbrsnps2","cnt1","cnt2","pi1","pi2","dpi","Ocnt1","Ocnt2","Opi1","Opi2","fet_or","fetp","chisq","chip"]
-    np.savetxt("%s%s_%s_pi.txt"%(args.prefix,pop1.ix[0,"structid"],pop1.ix[0,"biounit"]),res[~np.isnan(res[:,-1].astype(np.float64))],fmt="%s",delimiter='\t',header='\t'.join(names),comments="#")
+    res   = np.vstack((pop1.iloc[:,1:7].values.T,sph1["nbrsnps"].T,sph2["nbrsnps"].T,sph1["snpcnt"].T,sph2["snpcnt"].T,cnt1,cnt2,pi1,pi2,dpi,Ocnt1,Ocnt2,Opi1,Opi2,fet.T,chisq.T)).T
+    if not np.isnan(res[:,-4].astype(np.float64)).all():
+      print "\nMinimum FET: %s"%res[np.nanargmin(res[:,-4].astype(np.float64)),:]
+      print "Maximum FET: %s\n"%res[np.nanargmax(res[:,-4].astype(np.float64)),:]
+    names  = ["structid","biounit","model","chain","seqid","icode","nbrsnps1","nbrsnps2","snpcnt1","snpcnt2","ac1","ac2","pi1","pi2","dpi","Ocnt1","Ocnt2","Opi1","Opi2","fet_or","fetp","chisq","chip"]
+    print "\nTotal spheres: %d"%res.shape[0]
+    print "\nFiltering %d empty spheres..."%((res[:,8]<1) & (res[:,9]<1)).sum()
+    print "\nRetaining %d populated spheres..."%((res[:,8]>0) | (res[:,9]>0)).sum()
+    print "\nWriting results to %s..."%"%s%s_%s_pi.txt"%(args.prefix,pop1.ix[0,"structid"],pop1.ix[0,"biounit"])
+    np.savetxt("%s%s_%s_pi.txt"%(args.prefix,pop1.ix[0,"structid"],pop1.ix[0,"biounit"]),
+              res[(res[:,8]>0) | (res[:,9]>0)],
+              fmt="%s",delimiter='\t',header='\t'.join(names),comments="#")
+    
   except Exception as e:
     print "Error in %s,%s"%(s1,s2)
     print str(e)
