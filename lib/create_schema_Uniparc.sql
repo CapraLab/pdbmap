8161d111af56229ad374ba81a5c97152e82641ce
ChrisMoth <chris.moth@vanderbilt.edu>
2019-04-01 15:04:10 -0500

    Parse the UniParc fasta file (#6), and add to pdbmap_14.Uniparc Table in chunks of 1000 INSERTs

---
+++ lib/create_schema_Uniparc.sql
@@ -0,0 +1,8 @@
+CREATE TABLE pdbmap_v14.Uniparc (
+  uniparc CHAR(13) NOT NULL COMMENT 'Uniparc identifer in format UPI1234567890123',
+  md5sum  CHAR(32) NOT NULL COMMENT '128 bit md5sum of the amino acid sequence',
+  fasta VARCHAR(60000) NOT NULL COMMENT 'Amino acid sequence',
+  PRIMARY KEY (uniparc),
+  UNIQUE (md5sum)
+  -- UNIQUE (fasta(100)) -- FOR NOW DO NOT INDEX ON the fasta string, because you lookup by md5sum - not AA string
+) ENGINE=InnoDB COMMENT 'Map between UniParc identifiers, fasta sequences, and their md5sums'
