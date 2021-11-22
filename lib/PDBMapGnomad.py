272f40a5d1816dcb1a9c4c2d4ff7046151cf3c84
ChrisMoth <chris.moth@vanderbilt.edu>
2021-11-22 14:37:01 -0600

    When retrieving Gnomad Neutral variants, skip ENST transcript which map to chrCHR error regions of the genome

--- lib/PDBMapGnomad.py
+++ lib/PDBMapGnomad.py
@@ -14,26 +14,26 @@
     ENST ENSEMBL transcript identifier"""
 
 # See main check for cmd line parsing
-import sys
+# import sys
 import os
-import gzip
+# import gzip
 import logging
-import string
-import vcf
+# import string
+# import vcf
 import pandas as pd
-from typing import Iterator, Dict
+from typing import Dict, Union
 import subprocess as sp
 from lib.PDBMapGlobals import PDBMapGlobals
-from lib.PDBMapSQLdb import PDBMapSQLdb
+# from lib.PDBMapSQLdb import PDBMapSQLdb
 from lib.PDBMapVEP import PDBMapVEP
 from lib.PDBMapTranscriptEnsembl import PDBMapTranscriptEnsembl
-from lib import bed  # PyVCF emulator for BED files
+# from lib import bed  # PyVCF emulator for BED files
 
 LOGGER = logging.getLogger(__name__)
 SQL_EXTENDED_LOCK_WAIT = "SET innodb_lock_wait_timeout=200;\n"
 
 
-class PDBMapGnomad():
+class PDBMapGnomad:
     """ Class PDBMapVep() returns the current set of Gnomad variants, for a given
         ENST ENSEMBL transcript identifier"""
 
@@ -52,45 +52,65 @@ class PDBMapGnomad():
         if not self._config_dict or 'gnomad_filename_template' not in self._config_dict:
             raise Exception("gnomad_filename_template= is missing from the config file(s), or invalid")
 
-    def retrieve_gnomad_missense_variants(self, transcript: PDBMapTranscriptEnsembl, vep_echo_filename = None, output_directory=None) -> pd.DataFrame:
-        (success,chrom,genomic_coordinates) =  transcript.load_chromosome_location()
+    def retrieve_gnomad_missense_variants(self,
+                                          transcript: PDBMapTranscriptEnsembl,
+                                          vep_echo_filename=None,
+                                          output_directory=None) -> Union[pd.DataFrame, None]:
+        """
+        Given an Ensembl Transcript, determine the range of referenced genomic coordinates
+        and query the Gnomad grch38 liftover database source files directly via tabix.
+        Then, run those variants through the variant effect predictor to filter for missense variants.
+
+
+        @transcript: Ensembl Transcript
+        @vep_echo_filename: Optional
+
+        @return: Dataframe of missense Gnomad variants variants
+        """
+
+        # Call the ENSEMBL PERL API to load genomic coordinates for the ensembl transcript
+        (success, chrom, genomic_coordinates) = transcript.load_chromosome_location()
         if not success or len(genomic_coordinates) < 1:
-            LOGGER.warning("Unable to load chromosome locations for %s",transcript)
+            LOGGER.warning("Unable to load chromosome locations for %s", transcript.id)
             return None
+
+        # The min and max coordinates fall out of the returned array from ENSEMBL PERL API
         min_coordinate = genomic_coordinates[1][1]
         max_coordinate = genomic_coordinates[len(genomic_coordinates)][2]
         LOGGER.info("Chrom: %s Coordinate range [%d,%d].  Excerpting vcf segment next with tabix.",
-                    chrom,min_coordinate,max_coordinate)
+                    chrom, min_coordinate, max_coordinate)
 
-        # The vcf filename from gnomad omits chr in chrnn/chrX/chrY
+        # The Gnomad supplied vcf filename omits chr in chrnn/chrX/chrY
         gnomad_vcf_file = os.path.join(
             self._config_dict['gnomad_dir'],
-            self._config_dict['gnomad_filename_template']%chrom[3:]
+            self._config_dict['gnomad_filename_template'] % chrom[3:]
         )
 
-        gnomad_vcf_excerpt_filename = "%s_%d_%d_tabix.vcf"%(chrom[3:],min_coordinate,max_coordinate)
+        # Excerpt the tiny (by comparison) chrom region referenced by the Ensembl transcript
+        gnomad_vcf_excerpt_filename = "%s_%d_%d_tabix.vcf" % (chrom[3:], min_coordinate, max_coordinate)
         if output_directory:
-            gnomad_vcf_excerpt_filename=os.path.join(
-                output_directory,gnomad_vcf_excerpt_filename)
+            gnomad_vcf_excerpt_filename = os.path.join(
+                output_directory, gnomad_vcf_excerpt_filename)
 
-        tabix_command = ['tabix','-h',
-                          gnomad_vcf_file,
-                          "%s:%d-%d"%(chrom,min_coordinate,max_coordinate)]
+        tabix_command = ['tabix', '-h',
+                         gnomad_vcf_file,
+                         "%s:%d-%d" % (chrom, min_coordinate, max_coordinate)]
 
-        LOGGER.info("Running tabix with:\n%s"%' '.join(tabix_command))
-        completed_process = None
-        with open(gnomad_vcf_excerpt_filename,'wb') as gnomad_vcf_excerpt_f:
-            completed_process = sp.run(tabix_command,stdout=gnomad_vcf_excerpt_f,timeout=240)
+        LOGGER.info("Running tabix with:\n%s" % ' '.join(tabix_command))
+        with open(gnomad_vcf_excerpt_filename, 'wb') as gnomad_vcf_excerpt_f:
+            completed_process = sp.run(tabix_command, stdout=gnomad_vcf_excerpt_f, timeout=240)
             completed_process.check_returncode()
 
+        # Sanity check things a bit but counting the lines excerpted from the Chrom coordinate range.
         linecount = 0
-       	with open(gnomad_vcf_excerpt_filename,'r') as gnomad_vcf_excerpt_f:
-            for line in gnomad_vcf_excerpt_f:
+        with open(gnomad_vcf_excerpt_filename, 'r') as gnomad_vcf_excerpt_f:
+            for _ in gnomad_vcf_excerpt_f:
                 linecount += 1
-             
-        LOGGER.info("%d lines in tabix excerpted file %s"%(linecount,gnomad_vcf_excerpt_filename))
 
-        # MAFs are critical to dealing with Gnomad consequences.  We extract those from the tabix excerpt 
+        LOGGER.info("%d lines in tabix excerpted file %s" % (linecount, gnomad_vcf_excerpt_filename))
+
+        # This chunk of code is on hold, ebecause we get the MAFs from after the VEP call now.
+        # MAFs are critical to dealing with Gnomad consequences.  We extract those from the tabix excerpt
         # gnomad_df = pd.DataFrame(columns=['gene', 'chrom', 'pos', 'maf'])
         # pdbmap_vep = PDBMapVEP(self._config_dict)
         # vcf_reader = pdbmap_vep.vcf_reader_from_file_without_vep(gnomad_vcf_excerpt_filename,prepend_chr=False)
@@ -103,9 +123,7 @@ class PDBMapGnomad():
         #             'maf': float(vcf_record.INFO['AF']) # Minor allele frequency
         #             }, ignore_index=True)
 
-
-
-        LOGGER.info("Running VEP to update transcript annotations. Will filter for %s"%transcript.id)
+        LOGGER.info("Running VEP to update transcript annotations. Will filter for %s" % transcript.id)
         pdbmap_vep = PDBMapVEP(self._config_dict)
 
         vcf_reader = pdbmap_vep.vcf_reader_from_file_supplemented_with_vep_outputs(
@@ -113,12 +131,17 @@ class PDBMapGnomad():
             vep_echo_filename=vep_echo_filename,
             prepend_chr=False)
 
-        df = pd.DataFrame(columns=['gene', 'chrom', 'pos', 'transcript','Ref_AminoAcid','Protein_position','Alt_AminoAcid' , 'maf'])
+        # We now reduce the gnomad columns from over 4000 to these here:
+        df = pd.DataFrame(columns=['gene', 'chrom', 'pos', 'transcript', 'Ref_AminoAcid',
+                                   'Protein_position', 'Alt_AminoAcid', 'maf'])
 
+        # Filter the VEP output records for missense variants, and build up a dataframe of all the collected
+        # missense variants, which will be returned to the caller.
         for vcf_record in pdbmap_vep.yield_completed_vcf_records(vcf_reader):
             for CSQ in vcf_record.CSQ:
                 vep_transcript = CSQ['Feature']
-                if str(vep_transcript).strip() == transcript.id and str(CSQ['Consequence']).lower().find('missense') != -1:
+                if str(vep_transcript).strip() == transcript.id and str(CSQ['Consequence']).lower().find(
+                        'missense') != -1:
                     df = df.append(
                         {'gene': CSQ['SYMBOL'],
                          'chrom': vcf_record.CHROM,
@@ -127,8 +150,7 @@ class PDBMapGnomad():
                          'Ref_AminoAcid': CSQ['Ref_AminoAcid'],
                          'Alt_AminoAcid': CSQ['Alt_AminoAcid'],
                          'Protein_position': CSQ['Protein_position'],
-                         'maf': float(vcf_record.INFO['AF']) # Minor allele frequency
+                         'maf': float(vcf_record.INFO['AF'])  # Minor allele frequency
                          }, ignore_index=True)
-        LOGGER.info("%d raw missense variants excerpted from VCF fragment for %s"%(len(df),transcript.id))
+        LOGGER.info("%d raw missense variants excerpted from VCF fragment for %s" % (len(df), transcript.id))
         return df
-
--- lib/PDBMapTranscriptEnsembl.py
+++ lib/PDBMapTranscriptEnsembl.py
@@ -10,7 +10,7 @@
 # Date           : 2019-0618-
 # Description    : Class PDBMapTranscriptEnsembl.py accesses and encapsulates
 #                : everything we can know about one ENST....  transcript id
-#=============================================================================#
+# =============================================================================#
 
 # See main check for cmd line parsing
 import sys
@@ -19,9 +19,12 @@ import re
 import subprocess
 from Bio.Data import IUPACData
 from lib import PDBMapTranscriptBase
+from typing import Tuple
 
 import logging
+
 LOGGER = logging.getLogger(__name__)
+
 
 class PDBMapTranscriptEnsembl(PDBMapTranscriptBase):
     """Encapsulate ENST... transcript IDs and their mappings to amino acid sequences
@@ -35,220 +38,209 @@ class PDBMapTranscriptEnsembl(PDBMapTranscriptBase):
 
     @classmethod
     def __init__ensembl_registry__(cls):
-        if cls._ensembl_registry: # Don't re-initialize
+        if cls._ensembl_registry:  # Don't re-initialize
             return
         # One global variable we'd like to tease out is the
         # ENSEMBL_REGISTRY and specific dbname (containing Genome and Ensembl API version)
         cls._ensembl_registry = os.getenv("ENSEMBL_REGISTRY")
         if not cls._ensembl_registry:
-            fail_message="ENSEMBL_REGISTRY environment variable requried for PERL api configuration"
+            fail_message = "ENSEMBL_REGISTRY environment variable requried for PERL api configuration"
             LOGGER.critical(fail_message)
             sys.exit(fail_message)
         cls._ensembl_api_version = None
         cls._ensembl_genome_version = None
         if cls._ensembl_registry:
-            with open(cls._ensembl_registry,'r') as file:
+            with open(cls._ensembl_registry, 'r') as file:
                 for line in file:
                     if '-dbname' in line and 'homo_sapiens_core_' in line and '=>' in line:
                         # Tease out ensembl API and Genome from the
                         # dbname => homo_sapiens_core_API_GENOME   string in the registry file
-                        match_object = re.match(".*dbname.*homo_sapiens_core_(\\d*)_(\\d*).*",line)
+                        match_object = re.match(".*dbname.*homo_sapiens_core_(\\d*)_(\\d*).*", line)
                         if match_object:
-                            cls._ensembl_api_version = (int)(match_object.group(1))
-                            cls._ensembl_genome_version = (int)(match_object.group(2))
+                            cls._ensembl_api_version = int(match_object.group(1))
+                            cls._ensembl_genome_version = int(match_object.group(2))
 
         if not (cls._ensembl_api_version and cls._ensembl_genome_version):
-            LOGGER.critical("registry file %s lacks a -dbname => 'homo_sapiens_core_API_GENOME' entry"%cls._ensembl_registry)
+            LOGGER.critical(
+                "registry file %s lacks a -dbname => 'homo_sapiens_core_API_GENOME' entry" % cls._ensembl_registry)
             sys.exit(1)
 
-    def __init__(self,ensembl_ENST):
+    def __init__(self, ensembl_ENST_transcript_id):
         """An ENSTnnnnnn ENSEMBL transcript ID is required to instantiate"""
         # Define transcript, gene, and sequence
         if not PDBMapTranscriptEnsembl._ensembl_registry:
             PDBMapTranscriptEnsembl.__init__ensembl_registry__()
 
-        self.ensembl_ENST = ensembl_ENST
-        self.ensembl_ENSG = None       # ENSG* gene identifer mapped to this ENST transcript
-        self.ensembl_ENSP = None       # ENSP* protein id mapped to this ENST transcript
-        self.ensembl_chromosome = None # Chromosome 'chr*' returned from the ensembl API
-        self.ensembl_strand = None     # 1 or -1, depending on forward or reverse strand
-        
+        self._ensembl_ENST = ensembl_ENST_transcript_id
+        self.ensembl_ENSG = None  # ENSG* gene identifer mapped to this ENST transcript
+        self.ensembl_ENSP = None  # ENSP* protein id mapped to this ENST transcript
+        self.ensembl_chromosome = None  # Chromosome 'chr*' returned from the ensembl API
+        self.ensembl_strand = None  # 1 or -1, depending on forward or reverse strand
+
         # genome_locations is a dictionary leyed of transcript location
         # Sequence | key:   transcript seqid
         # Sequence | value: (aa_letter,start,end,strand)
-        self.ensembl_sequence = {} # ensembl_sequence[store genomic sequence keyed on genome location
-        super().__init__(None,None) # Set self._aa_seq and self._uniparc_id to None for starters
+        self.ensembl_sequence = {}  # ensembl_sequence[store genomic sequence keyed on genome location
+        super().__init__(None, None)  # Set self._aa_seq and self._uniparc_id to None for starters
 
     @property
     def id(self):
-        return self.ensembl_ENST
+        return self._ensembl_ENST
 
     @property
     def aa_seq(self):
         if not self._aa_seq:
-            (result,aa_seq) = self.load_aa_seq_from_ENSEMBL()
-            if not result: # Then aa_seq contains an error message
-                msg = "ENSMBL API failed to return aa_seq for %s: %s"%(self.ensembl_ENST,aa_seq)
+            (result, aa_seq) = self.load_aa_seq_from_ENSEMBL()
+            if not result:  # Then aa_seq contains an error message
+                msg = "ENSMBL API failed to return aa_seq for %s: %s" % (self._ensembl_ENST, aa_seq)
                 LOGGER.critical(msg)
                 return None
         return self._aa_seq
 
-    def is_standard_chromosome():
+    def is_standard_chromosome(self):
         if not self.ensembl_chromosome:
             LOGGER.error("Chromosome chr* string must be first loaded from ENSMBL Api call")
             return False
-           
+
         chrom_suffix = ''
-        if chrom[0:3] == 'chr':
-            chrom_suffsx = chrom[3]
+        if self.ensembl_chromosome[0:3] == 'chr':
+            chrom_suffix = self.ensembl_chromosome[3]
         # Make sure the chromosome is chr1 to chr22 OR chrX, chrY, chrMT
         # If transcript on a haplotype chromosome, ignore
-        return ( (chrom_suffix.isdigit() and int(chrom_suffix) <= 22 and chrom_suffix[0] >= '1') or
-                 (chrom_suffix in ['X','Y','MT']) )
+        return ((chrom_suffix.isdigit() and int(chrom_suffix) <= 22 and chrom_suffix[0] >= '1') or
+                (chrom_suffix in ['X', 'Y', 'MT']))
 
         #    LOGGER.warn("Ignoring non-standard chromosome %s returned from \'%s\' command output"%(chrom,cmd))
 
-    def _run_perl_script(self,script_filename):
-        # cmd_list = ["perl", "lib/%s"%script_filename, self.ensembl_ENST]
-        cmd_list = ["%s"%script_filename, self.ensembl_ENST]
+    def _run_perl_script(self, script_filename: str) -> subprocess.CompletedProcess:
+        """
+        Run one of the ...pl perl scripts that interfaces to the ENSEMBL Perl API.
+        The one argument is simply the ENST transcript id
+        """
+        cmd_list = ["%s" % script_filename, self._ensembl_ENST]
         cmd_string = ' '.join(cmd_list)
-        LOGGER.info("Launching PERL script to access ENSEMBL: %s"%cmd_string)
-        completed_process = subprocess.run(cmd_list,encoding='utf-8',stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+        LOGGER.info("Launching PERL script to access ENSEMBL: %s" % cmd_string)
+        completed_process = subprocess.run(cmd_list, encoding='utf-8', stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         if completed_process.returncode != 0:
-            LOGGER.warning("Exit Code %d returned from: %s\nstderr: %s\nstdout:%s"%(
+            LOGGER.warning("Exit Code %d returned from: %s\nstderr: %s\nstdout:%s" % (
                 completed_process.returncode,
                 cmd_string,
-                completed_process.stderr.rstrip(),completed_process.stdout.rstrip()))
+                completed_process.stderr.rstrip(), completed_process.stdout.rstrip()))
         return completed_process
 
     # Quick sanity check to ensure returned aa_seq makes some sense
-    def _aa_seq_check(self):
-        if not self._aa_seq: # MAjor fail of calling software
+    def _aa_seq_check(self) -> Tuple[bool, str]:
+        if not self._aa_seq:  # MAjor fail of calling software
             LOGGER.critical("AAseq not yet loaded")
             sys.exit(1)
 
-        valid_aa_letters = set(IUPACData.protein_letters) # Most standard 20
-        valid_aa_letters.add('U') # Deal with Selenomet
+        valid_aa_letters = set(IUPACData.protein_letters)  # Most standard 20
+        valid_aa_letters.add('U')  # Deal with Selenomet
         all_letters_ok = all(aa in valid_aa_letters for aa in self.aa_seq)
         if all_letters_ok:
-            return (True,self.aa_seq)
-        return (False,"Bad AA letters are %s"%str([aa for aa in self.aa_seq if aa not in valid_aa_letters]))
+            return True, self.aa_seq
+        return False, "Bad AA letters are %s" % str([aa for aa in self.aa_seq if aa not in valid_aa_letters])
 
-    def load_aa_seq_from_ENSEMBL(self):
+    def load_aa_seq_from_ENSEMBL(self) -> Tuple[bool, str]:
         """Run lib/transcript_to_AAseq.pl to retrieve aa_seq from ENST... id"""
         if self._aa_seq:
             LOGGER.info("aa_seq already loaded.  Returning from load_aa_seq_from_ENSEMBL immediately")
-            return (True,self.aa_seq)
+            return True, self.aa_seq
         completed_process = self._run_perl_script("transcript_to_AAseq.pl")
         if completed_process.returncode != 0:
-            return (False,completed_process.stderr.rstrip() + "(GRCh%s v %s)"%(PDBMapTranscriptEnsembl._ensembl_genome_version,PDBMapTranscriptEnsembl._ensembl_api_version))
+            return False, completed_process.stderr.rstrip() + "(GRCh%s v %s)" % (
+                PDBMapTranscriptEnsembl._ensembl_genome_version, PDBMapTranscriptEnsembl._ensembl_api_version)
         self._aa_seq = completed_process.stdout.rstrip()
         return self._aa_seq_check()
-        
 
     def load_chromosome_location(self):
-        """ Load genomic locations for the ensembl_ENST via the ENSEMBL PERL API """
+        """ Load genomic locations for the _ensembl_ENST via the ENSEMBL PERL API """
         if self.ensembl_sequence and self.ensembl_chromosome:
             LOGGER.info("chromosome location already loaded.  Returning from load_chromosome_location immediately")
-            return (True,self.ensembl_chromosome,self.ensembl_sequence)
-  
+            return True, self.ensembl_chromosome, self.ensembl_sequence
+
         completed_process = self._run_perl_script("transcript_to_genomic.pl")
         if completed_process.returncode != 0:
-            return (False,completed_process.stderr.rstrip(),None)
+            return False, completed_process.stderr.rstrip(), None
         lineno = 1
         for line in completed_process.stdout.split('\n'):
-            if len(line) < 1: continue # It's nothing 
-            print (lineno,line)
+            if len(line) < 1:  # It's nothing
+                continue
+            LOGGER.debug("Line %3d: %s", lineno, line)
             lineno += 1
-            if line.startswith('#'): continue # It's a comment
+            if line.startswith('#'):  # It's a comment
+                continue
             fields = line.split('\t')
-            if len(fields) < 2: # Let's not be upset with a blank line
+            if len(fields) < 2:  # Let's not be upset with a blank line
                 continue
-            if len(fields) < 9: # Let's report on lines that don't make sense
-                LOGGER.warn("Incomplete line returned from transcript_to_genomic.pl:\n[%s]"%line)
+            if len(fields) < 9:  # Let's report on lines that don't make sense
+                LOGGER.warning("Incomplete line returned from transcript_to_genomic.pl:\n[%s]" % line)
                 continue
-            transcript = fields[0]
 
+            # The left column of returned values should match our requested ENST transcript id
+            transcript = fields[0]
+            assert transcript == self._ensembl_ENST
+
+            # The ENSP Protein identifier shold be the same for all liness returned from the PERL API
             if self.ensembl_ENSP:
-                assert(self.ensembl_ENSP == fields[1].strip())
+                assert (self.ensembl_ENSP == fields[1].strip())
             else:
                 self.ensembl_ENSP = fields[1].strip()
 
+            # The ENSP Gene identifier shold be the same for all lines returned by the PERL API
             if self.ensembl_ENSG:
-                assert(self.ensembl_ENSG == fields[2].strip())
+                assert (self.ensembl_ENSG == fields[2].strip())
             else:
                 self.ensembl_ENSG = fields[2].strip()
 
+            # The ENSEMBL chrnn should be the same for all returned lines from the PERL API
+            # And, it must be one of the "standard" human chromosomes
             if self.ensembl_chromosome:
-                assert(self.ensembl_chromosome == fields[7].strip())
+                assert (self.ensembl_chromosome == fields[7].strip())
             else:
+                # If the returned chromosome is not a "standard" human , then we need to return failure to the caller
                 self.ensembl_chromosome = fields[7].strip()
+                if not self.is_standard_chromosome():
+                    bad_chr_format_message = "Unrecognized chr:%s  in line %d: %s" % (
+                        self.ensembl_chromosome,
+                        lineno,
+                        line)
+
+                    LOGGER.warning(bad_chr_format_message)
+                    return False, bad_chr_format_message, None
 
             if self.ensembl_strand:
                 assert (self.ensembl_strand == int(fields[8]))
             else:
                 self.ensembl_strand = int(fields[8])
 
-            transcript_index      = int(fields[3]) # 1 to N - the index of the aa transcript
-            aa_letter    = fields[4].upper()
-            if not aa_letter in IUPACData.protein_letters:
-                aa_letter  = 'X' # replace non-standard amino acids with X
-            start      = int(fields[5])
-            end        = int(fields[6])
+            transcript_index = int(fields[3])  # 1 to N - the index of the aa transcript
+            aa_letter = fields[4].upper()
+            if aa_letter not in IUPACData.protein_letters:
+                aa_letter = 'X'  # replace non-standard amino acids with X
+            start = int(fields[5])  # Genomic position on the chromosome where this AA starts
+            end = int(fields[6])  # Genomic position on the chromosome where this AA ends
 
-
-            self.ensembl_sequence[transcript_index] = (aa_letter,start,end)
+            # It is convenient to build up the AA sequence without doing the other PERL API call
+            self.ensembl_sequence[transcript_index] = (aa_letter, start, end)
 
         if not self.ensembl_sequence:
-            return (False,"No interpretable data returned from ENSEMBL PERL API transcript_to_genomic.pl: %s"%completed_process.stdout.rstrip(),None)
+            return False, "No interpretable data returned from ENSEMBL PERL API transcript_to_genomic.pl: %s" % \
+                        completed_process.stdout.rstrip(), None
+
         residue_numbers = sorted(self.ensembl_sequence.keys())
         if residue_numbers[0] != 1:
-            return (False,"Amino Acid for residue #1 was not returned by transcript_to_genomic.pl",None)
+            return False, "Amino Acid for residue #1 was not returned by transcript_to_genomic.pl", None
         if residue_numbers[-1] != len(residue_numbers):
-            return (False,"Last residue number returned by transcript_to_genomic.pl does not match residue count",None)
+            return False, "Last residue number returned by transcript_to_genomic.pl does not match residue count", None
 
+        # Update the internal aa_seq string with these aa_letters
         self._aa_seq = ''.join([self.ensembl_sequence[transcript_index][0] for transcript_index in residue_numbers])
 
-        return (True,self.ensembl_chromosome,self.ensembl_sequence)
+        return True, self.ensembl_chromosome, self.ensembl_sequence
 
-    @classmethod
-    def cache_transcript(cls,transid,transcript):
-      """ Caches a transcript result from a transid query """
-      # Cache the transid->transcript query
-      PDBMapTranscript.trans_cache[transid] = (transcript,0)
-      # Update access counts
-      for tid,(tobj,count) in PDBMapTranscript.trans_cache.items():
-        PDBMapTranscript.trans_cache[tid] = (tobj,count+1)
-      # Remove infrequently accessed entries
-      PDBMapTranscript.trans_cache = dict([(x,(y,z)) for x,(y,z) in \
-                  PDBMapTranscript.trans_cache.items() \
-                if z < PDBMapTranscript.CACHE_ACCESS_MIN])
-
-aa_code_map = {"ala" : "A",
-        "arg" : "R",
-        "asn" : "N",
-        "asp" : "D",
-        "asx" : "B",
-        "cys" : "C",
-        "glu" : "E",
-        "gln" : "Q",
-        "glx" : "Z",
-        "gly" : "G",
-        "his" : "H",
-        "ile" : "I",
-        "leu" : "L",
-        "lys" : "K",
-        "met" : "M",
-        "phe" : "F",
-        "pro" : "P",
-        "ser" : "S",
-        "thr" : "T",
-        "trp" : "W",
-        "tyr" : "Y",
-        "val" : "V",
-        "unk" : "X"}
 
 # Main check
-if __name__== "__main__":
-  LOGGER.critical("Class definition. Should not be called from command line.")
-  sys.exit(1)
+if __name__ == "__main__":
+    LOGGER.critical("Class definition. Should not be called from command line.")
+    sys.exit(1)
