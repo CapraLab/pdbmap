b88dd4c818702d1a99e9ce00d3aa9a04a39543c9
ChrisMoth <chris.moth@vanderbilt.edu>
2022-06-20 13:23:29 -0500

    Speed up uniparc tables and create human-only spinoff #36

--- lib/create_schema_Uniparc.sql
+++ lib/create_schema_Uniparc.sql
@@ -1,12 +1,26 @@
 -- After Creating the Table, populate it with the pdbmap/scripts/uniparc_parser.py
 -- Importantly, the Uniparc identifiers are invariant.  Thus, you should not DROP the table
 -- before running the script
+--
+-- NOTE THAT the fasta sequence can be quite long, and MEDIUMTEXT seems to work on MariaDB 5 and 10 alike
+--      VARCHAR(60000) seems to cause all kinds of problems with JOINs by comparison
 
 CREATE TABLE pdbmap_v14.Uniparc (
-  uniparc CHAR(13) NOT NULL COMMENT 'Uniparc identifer in format UPI1234567890123',
-  md5sum  CHAR(32) NOT NULL COMMENT '128 bit md5sum of the amino acid sequence',
-  fasta VARCHAR(60000) NOT NULL COMMENT 'Amino acid sequence',
+  uniparc VARCHAR(20) NOT NULL COMMENT 'Uniparc identifer in format UPI1234567890123',
+  md5sum  VARCHAR(32) NOT NULL COMMENT '128 bit md5sum of the amino acid sequence',
+  fasta MEDIUMTEXT NOT NULL COMMENT 'Amino acid sequence',
   PRIMARY KEY (uniparc),
   UNIQUE (md5sum)  -- The UNIQUE keyword will cause an INDEX to be create on the md5sum
   -- UNIQUE (fasta(100)) -- FOR NOW DO NOT INDEX ON the fasta string, because you lookup by md5sum - not AA string
 ) ENGINE=InnoDB COMMENT 'Map between UniParc identifiers, fasta sequences, and their md5sums'
+CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+
+CREATE TABLE pdbmap_v14.UniparcHuman (
+  uniparc VARCHAR(20) NOT NULL COMMENT 'Uniparc identifer in format UPI1234567890123',
+  md5sum  VARCHAR(32) NOT NULL COMMENT '128 bit md5sum of the amino acid sequence',
+  fasta MEDIUMTEXT NOT NULL COMMENT 'Amino acid sequence',
+  PRIMARY KEY (uniparc),
+  UNIQUE (md5sum)  -- The UNIQUE keyword will cause an INDEX to be create on the md5sum
+  -- UNIQUE (fasta(100)) -- FOR NOW DO NOT INDEX ON the fasta string, because you lookup by md5sum - not AA string
+) ENGINE=InnoDB COMMENT 'Map between UniParc identifiers, fasta sequences, and their md5sums'
+CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
--- scripts/uniparc_parser.py
+++ scripts/uniparc_parser.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # Parses a (large!) uniparc cross reference file
 # and uploads data to mysql database
+# The --humanonly option dramatically reduces output table size
+# By only unicluding sequences of unipqrc IDs seen in our curated swissprot idmapping file
 
 from collections import defaultdict
 import sys, os, glob, re, gzip
@@ -15,13 +17,26 @@ cmdline_parser.add_argument("no_per_insert", nargs="?", help="Count of uniparcs 
                             default=5000, type=int)
 cmdline_parser.add_argument("-c", "--conf_file", required=True, help="Specify database config file", metavar="FILE")
 cmdline_parser.add_argument("--skip_until_id", required=False, help="To speed restarts, specify a 'known uploaded through' UNIPARC id", type=str, metavar="str")
+cmdline_parser.add_argument("--humanonly", required=False, help="Add only human uniparc IDs to UniparcHuman table", action='store_true')
 args = cmdline_parser.parse_args()
+
 
 config = configparser.ConfigParser()
 config.read([args.conf_file])
 
 configdict = dict(config.items("Genome_PDB_Mapper"))
 
+human_uniparc_ids = set()
+if args.humanonly:
+   print("--humanonly set: Only uniparc IDs referenced in %s will be added to UniparcHuman" % configdict['idmapping'])
+   with gzip.open(configdict['idmapping'],'rt') as idmapping_f:
+       for line in idmapping_f:
+           unp_ID_type_ID = line.split('\t')
+           if unp_ID_type_ID[1] == 'UniParc':
+               human_uniparc_ids.add(unp_ID_type_ID[2].strip())
+   print("%d Human uniparc ids will be added" % len(human_uniparc_ids))
+else:
+   print("--humanonly not set: All uniparcc IDs will be added to the Uniparc table")
 # Connect to the databsae
 import MySQLdb
 
@@ -42,7 +57,8 @@ def flush_uniparcs(uniparc_dict):
 
     ## Upload to database
     c = con.cursor()
-    sql = "INSERT IGNORE INTO pdbmap_v14.Uniparc (uniparc,md5sum,fasta) VALUES (%s, %s, %s)"
+    sql = "INSERT IGNORE INTO pdbmap_v14.%s (uniparc,md5sum,fasta) VALUES (%%s, %%s, %%s)" % (
+        "UniparcHuman" if args.humanonly else "Uniparc",)
 
     try:
         print("Uploading %d items ranging %s to %s" % (len(insert_list), insert_list[0][0], insert_list[-1][0]),
@@ -65,7 +81,8 @@ with gzip.open(args.uniparc_filename, "rt") as f:
     cur_is_active = False # True for most entries, where "status=active"
     uniparc_dict = {}
     skipping = True if args.skip_until_id else False
-    print("Skipping until uniparc ID %s is encountered" % args.skip_until_id)
+    if skipping:
+        print("Skipping until uniparc ID %s is encountered" % args.skip_until_id)
     for line in f:
         # When we get to the next >UPI... string
         if len(line) > 10 and line[0] == '>':
@@ -76,12 +93,13 @@ with gzip.open(args.uniparc_filename, "rt") as f:
                         skipping = False
 
                 if not skipping: # This is usual case - add the uniparc ID and sequence to SQL
-                    uniparc_dict[cur_uniparc] = cur_fasta;
+                    if not args.humanonly or cur_uniparc in human_uniparc_ids:
+                        uniparc_dict[cur_uniparc] = cur_fasta;
                 
-                    # Flush to SQL if we reach the buffer size limit
-                    if len(uniparc_dict) >= args.no_per_insert:
-                        flush_uniparcs(uniparc_dict)
-                        uniparc_dict = {}
+                        # Flush to SQL if we reach the buffer size limit
+                        if len(uniparc_dict) >= args.no_per_insert:
+                            flush_uniparcs(uniparc_dict)
+                            uniparc_dict = {}
 
             assert (line[1:4] == "UPI")
             cur_uniparc = line[1:14]  # UPI + 10 character unique ID make the identifier
