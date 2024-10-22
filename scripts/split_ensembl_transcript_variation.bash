a5763a789b831a008572e5decb3b8e1b3b8d7211
Chris Moth <chris.moth@vanderbilt.edu>
2024-10-22 15:41:35 -0500

    Improve script ability to deal with massive ENSEMBL transcript_variation table #41

--- scripts/split_ensembl_transcript_variation.bash
+++ scripts/split_ensembl_transcript_variation.bash
@@ -1,9 +1,11 @@
 # Every time we get new ensembl database, the very huge files must be split to have any sense of progress.
 echo WARNING DO NOT CHECK IN WITH MYSQLIMPORT PASSWORD IN COMMAND LINE
 version="108_38"
+download_date="2024-01-19"
+replace_With_pw="obfucate"
 echo ENSEMBL version is $version
 echo  # cd to homo_sapiens_variation_108_38 from
-cd /dors/capra_lab/data/ensembl/2022-11-17/homo_sapiens_variation_$version
+cd /dors/capra_lab/data/ensembl/$download_date/homo_sapiens_variation_$version
 mkdir -p transcript_variation_split
 split --verbose -l 5000000 -a 4 transcript_variation.txt transcript_variation_split/transcript_variation.piece.
 # Now that we've split, carefully do mysqlimport of each of the split-out files.  Take care with passwords
@@ -14,7 +16,7 @@ do
 echo $transcript_variation_piece
 mv --verbose $transcript_variation_piece transcript_variation.txt
 echo $transcript_variation_piece ": begin mysqlimport"
-mysqlimport --fields-terminated-by='\t' --fields-escaped-by=\\ homo_sapiens_variation_$version -preplace_with_pw -L transcript_variation.txt | tee -a mysqlimport.log
+mysqlimport --fields-terminated-by='\t' --fields-escaped-by=\\ homo_sapiens_variation_$version -p$replace_with_pw -L transcript_variation.txt | tee -a mysqlimport.log
 if [$? -eq 0 ]; then
     echo "mysqlimport successfully returned 0.  Removing input piece file"
     rm --verbose transcript_variation.txt
